66f88341eaf928228eec33ec1ccaaecb6c203038
Guilhem Fauré <pro@guilhemfau.re>
2025-11-28 22:09:34 +0100

    fix(AffTypeD): taille hex

--- AffTypeD.v
+++ AffTypeD.v
@@ -2,7 +2,7 @@ module AffTypeD(bcd100, bcd10, bcd1, en100, en10, hex_d, hex100, hex10, hex1);
 
 input[3:0] bcd100, bcd10, bcd1;
 input en100, en10;
-output[3:0] hex_d, hex100, hex10, hex1;
+output[6:0] hex_d, hex100, hex10, hex1;
 
 Aff7Seg aff_d(4'hD, 1, hex_d);         // 0xD (13) pour "d"
 Aff7Seg aff100(bcd100, en100, hex100); // Centaines
--- LancerD.v
+++ LancerD.v
@@ -1,14 +1,14 @@
-module LancerD(clk, lancer, min_de, max_de, hex0, hex1);
+module LancerD(clk, lancer, min_de, max_de, hex10, hex1);
 
 input clk, lancer;
 input[6:0] min_de, max_de;
 
-output[6:0] hex0, hex1;
+output[6:0] hex10, hex1;
 
 wire[6:0] entropie, resultat;
 
 Cnt #(7, 100)cnt(clk, entropie);
 Result res(lancer, entropie, min_de, max_de, resultat);
-AffResult aff(resultat, hex0, hex1);
+AffResult aff(resultat, hex10, hex1);
 
 endmodule
