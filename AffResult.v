f94c56834c02fe9f2299af59f2e84e8b535ad8ae
Guilhem Fauré <pro@guilhemfau.re>
2025-11-28 22:00:15 +0100

    fix: éteindre dizaines résultat si < 10

--- AffResult.v
+++ AffResult.v
@@ -3,7 +3,7 @@ module AffResult(result, hex10, hex1);
 input[6:0] result;
 output[6:0] hex10, hex1;
 
-Aff7Seg aff10(result / 10, 1, hex10);
+Aff7Seg aff10(result / 10, (result >= 10) ? 1 : 0, hex10);
 Aff7Seg aff1(result % 10, 1, hex1);
 
 endmodule
