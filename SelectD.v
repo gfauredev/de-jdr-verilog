ee35e2cae29809f8b9b84dda53a949f7c97ce77c
Guilhem Fauré <pro@guilhemfau.re>
2025-11-28 21:27:17 +0100

    refactor: clean, nouveau projet

--- SelectD.v
+++ SelectD.v
@@ -7,7 +7,7 @@ output[6:0] hex0, hex1, hex2, hex3;
 wire[2:0] id_de;
 wire en0, en1;
 
-cnt #(3, 8, 0)cntTypeD(suivant, id_de);
+Cnt #(3, 8, 0)cntTypeD(suivant, id_de);
 BornerD borner(id_de, min_de, max_de);
 ConvertBornesAffichage convert(min_de, max_de, bcd0, bcd1, bcd2, en0, en1);
 AffTypeD aff(bcd0, bcd1, bcd2, en0, en1, hex0, hex1, hex2, hex3);
