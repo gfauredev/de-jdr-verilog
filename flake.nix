6385d39be1383c64065d512eb1b499ad72690cf1
Guilhem Fauré <pro@guilhemfau.re>
2025-11-29 22:01:48 +0100

    feat: init nix env

---
+++ flake.nix
@@ -0,0 +1,38 @@
+{
+  description = "Nix flake Verilog development environment";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  outputs =
+    {
+      self,
+      nixpkgs,
+    }:
+    let
+      forEachSupportedSystem =
+        f:
+        nixpkgs.lib.genAttrs [
+          "x86_64-linux"
+          "aarch64-linux"
+        ] (system: f { pkgs = import nixpkgs { inherit system; }; });
+    in
+    {
+      devShells = forEachSupportedSystem (
+        { pkgs }:
+        {
+          default = pkgs.mkShell {
+            packages = with pkgs; [
+              # clang-tools # Clang CLIs
+              # cmake # Automation tool
+              # cmake-language-server # LSP
+              # doxygen # Documentation generator
+              # gnumake # Automation tool
+              iverilog # Icarus Verilog compiler
+              quartus-prime-lite # Main IDE
+              svls # SystemVerilog LSP
+              verilator
+              vhd2vl
+            ];
+          };
+        }
+      );
+    };
+}
